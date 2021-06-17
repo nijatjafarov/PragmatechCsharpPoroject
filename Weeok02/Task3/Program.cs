e02adffbaa11e0c6e617a82f547682bc8bc531c0
Nicat Ceferov <nicat.ceferov14@gmail.com>
2021-06-17 16:14:37 +0400

    Week02 Task3 was added

---
+++ Weeok02/Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a random integer number, keep calm and just watch what a crazy thing will happen: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if(number % 2 == 0)
+            {
+                Console.WriteLine("You entered an even number");
+            }
+            else if(number % 2 == 1)
+            {
+                Console.WriteLine("You entered an odd number");
+            }
+            else
+            {
+                Console.WriteLine("IDK man. This is life. Everything can be possible");
+            }
+
+            Console.WriteLine("Fantastic, right?");
+            Console.ReadLine();
+        }
+    }
+}
