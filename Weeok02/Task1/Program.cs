b8604c54f928e44d89c439e2fdb8f5f57422e259
Nicat Ceferov <nicat.ceferov14@gmail.com>
2021-06-17 15:58:12 +0400

    Week02 Task1 was added

---
+++ Weeok02/Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task1
+{
+    public struct User
+    {
+        public string Username;
+        public int Password;
+
+        public User(string username, int password)
+        {
+            Username = username;
+            Password = password;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User("nijatjafarov", 123456);
+
+            Console.Write("UserName daxil edin: ");
+            string userName = Console.ReadLine();
+
+            Console.Write("PassWord daxil edin: ");
+            int passWord = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            if(userName == user.Username && passWord == user.Password)
+            {
+                Console.WriteLine("Sayta xosh gelmishsiniz! Hemise siz gelesiniz!");
+            }
+            else
+            {
+                Console.WriteLine("UserName ve ya PassWord yalnishdir");
+            }
+        }
+    }
+}
