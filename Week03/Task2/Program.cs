214f2d53d557c8756b569f349a830e6d4d17ded4
Nicat Ceferov <nicat.ceferov14@gmail.com>
2021-06-21 23:10:49 +0400

    Added Week03 Task2

---
+++ Week03/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, sum = 0;
+            do
+            {
+                Console.Write("Enter a number: ");
+                num = int.Parse(Console.ReadLine());
+                sum += num;
+            } while (num != 0);
+
+            Console.WriteLine("Sum of entered numbers is: {0}", sum);
+        }
+    }
+}
