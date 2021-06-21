f7adefa7af4e9cc3d2167b03e1800548bce389f5
Nicat Ceferov <nicat.ceferov14@gmail.com>
2021-06-21 23:08:21 +0400

    Added Week03 Task1

---
+++ Week03/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int evenNum = 0;
+            int sum = evenNum;
+
+            while (evenNum < 100)
+            {
+                evenNum += 2;
+                sum += evenNum;
+                Console.WriteLine($"Even number: {evenNum} | Sum till this number: {sum}");
+            }
+            Console.WriteLine($"\nFinal sum is {sum}");
+        }
+    }
+}
