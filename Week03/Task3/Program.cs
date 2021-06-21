91b28fc387f014f826dd31e2fb1dbc2053a76b48
Nicat Ceferov <nicat.ceferov14@gmail.com>
2021-06-21 23:12:29 +0400

    Added Week03 Task3

---
+++ Week03/Task3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int randomNum = rand.Next(1, 10);
+            int guessedNum;
+            int totalGuesses = 0;
+
+            do
+            {
+                Console.Write("Try to guess the random number which computer selected: ");
+                guessedNum = int.Parse(Console.ReadLine());
+                totalGuesses += 1;
+
+            } while (totalGuesses < 5 && randomNum != guessedNum);
+
+            if (totalGuesses >= 5)
+            {
+                Console.WriteLine("You used all of your chances");
+            }
+            else if (randomNum == guessedNum)
+            {
+                Console.WriteLine("Congratulations! You found out the number");
+            }
+        }
+    }
+}
