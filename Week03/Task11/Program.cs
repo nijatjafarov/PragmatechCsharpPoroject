e0b4c3fd27a8ff6dcb9c439aa51c0940cd1133bf
Nicat Ceferov <nicat.ceferov14@gmail.com>
2021-06-25 23:38:46 +0400

    Added Week03 Task11

---
+++ Week03/Task11/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Bele etme sebebim proqramin biraz daha yungul
+            islemesini temin etmekdir, cut ededler onsuzda sade olmadigi ucun tekler uzerinden yoxladim ve
+            ilk basda 2 ededini mexaniki formada daxil etdim*/
+
+            bool isPrime = true;
+
+            Console.WriteLine(2);
+            for (int i = 3; i < 100; i += 2)
+            {
+                for (int j = 3; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    Console.WriteLine($"{i}");
+                }
+                isPrime = true;
+            }
+        }
+    }
+}
