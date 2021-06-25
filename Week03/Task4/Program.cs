aac4ee726aca8d60e7c2a13985d8531bfe8ac594
Nicat Ceferov <nicat.ceferov14@gmail.com>
2021-06-25 23:02:12 +0400

    Added Week03 Task4

---
+++ Week03/Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int factorial = 1;
+
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            while (number == 0)
+            {
+                Console.Write("Number can't be zero. Try again: ");
+                number = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 1; i < number + 1; i++)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine($"Factorial of {number} is {factorial}");
+
+        }
+    }
+}
