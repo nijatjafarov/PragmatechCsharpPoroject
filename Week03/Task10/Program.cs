4857763f45947531e329528194206cff2df4edf8
Nicat Ceferov <nicat.ceferov14@gmail.com>
2021-06-25 23:36:17 +0400

    Added Week03 Task10

---
+++ Week03/Task10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                Console.WriteLine(alphabet[i]);
+            }
+
+            Console.WriteLine("Vurma cedveli");
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine($"\n{i} ile vurma, ingilisce yazarsan");
+
+                for (int j = 1; j < 10; j++)
+                {
+                    Console.WriteLine($"{i} * {j} = {i * j}");
+                }
+            }
+        }
+    }
+}
