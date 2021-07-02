af7ea9cab886a793fcb3473b31ab5e87951b3807
Nicat Ceferov <nicat.ceferov14@gmail.com>
2021-07-02 19:53:03 +0400

    Added Week04 Task6

---
+++ Week04/Task6/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //First version
+            //Console.Write("How many numbers do you want to add to array? ");
+            //int length = int.Parse(Console.ReadLine());
+
+            //int[] nums = new int[length];
+            //int min;
+
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    Console.Write($"Add {i + 1}. number: ");
+            //    nums[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //min = nums[0];
+
+            //Console.Write("Your array | ");
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    Console.Write($"{nums[i]} ");
+            //    if (min > nums[i])
+            //    {
+            //        min = nums[i];
+            //    }
+            //}
+
+            //Console.WriteLine($"Minimum number in array is {min}");
+
+
+
+
+            //Second version
+            Console.Write("How many numbers do you want to add to array? ");
+            int length = int.Parse(Console.ReadLine());
+
+            int[] nums = new int[length];
+            int min;
+
+
+            Console.Write($"Add 1. number: ");
+            nums[0] = int.Parse(Console.ReadLine());
+            min = nums[0];
+
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                Console.Write($"Add {i + 1}. number: ");
+                int currentNum = int.Parse(Console.ReadLine());
+
+                nums[i] = currentNum;
+
+                if (min > currentNum)
+                {
+                    min = currentNum;
+                }
+            }
+
+            Console.Write("Your array | ");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write($"{nums[i]} ");
+            }
+
+            Console.WriteLine($"Minimum number in array is {min}");
+
+        }
+    }
+}
